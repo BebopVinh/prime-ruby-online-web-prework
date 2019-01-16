042090ae0821c127f8560006c0fda3d199fed228
Vinh Do <vinhdo07@gmail.com>
2019-01-16 03:10:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,15 @@
 # Add  code here!
 def prime?(x)
-  for i in (2.. i <= x/2)
-    if (x%2 == 0)
-      false
-      break
-    else
-      true
+  if x <= 1
+    false
+  else
+    for i in (2.. i <= x/2)
+      if (x%2 == 0)
+        false
+        break
+      else
+        true
+      end
     end
   end
 end
