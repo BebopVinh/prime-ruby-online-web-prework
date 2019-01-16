710ab73f5bfd2f2050c687c0256c2a585d254509
Vinh Do <vinhdo07@gmail.com>
2019-01-16 03:25:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,18 +2,8 @@
 def prime?(x)
   if x <= 1
     false
-  else
-    array = (2.. x/2).to_a
-    blank_array = []
-    array.each { |x| blank_array << x%2 }
-    for i in (0..blank_array.size)
-      if blank_array[i] == 0
-        return false
-        break
-      else
-        return true
-        break
-      end
-    end
+  elsif (x % 2 == 0 || x % 3 == 0)
+
+
   end
 end
