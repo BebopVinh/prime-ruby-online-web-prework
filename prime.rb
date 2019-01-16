fd5b82eac0714b4223c3e7e38afe2c069e6466db
Vinh Do <vinhdo07@gmail.com>
2019-01-16 03:18:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,5 +2,11 @@
 def prime?(x)
   array = (2.. x/2).to_a
   blank_array = []
-  array.each {|x| blank_array << x%2 }
+  array.each { |x| blank_array << x%2 }
+  for i in (0..blank_array.size)
+    if blank_array[i] == 0
+      return false
+      break
+    else
+      return true
 end
