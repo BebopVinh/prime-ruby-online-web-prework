c74b3f238d150757f9696c07848dc4d875282ba1
Vinh Do <vinhdo07@gmail.com>
2019-01-16 03:15:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,4 @@
 # Add  code here!
 def prime?(x)
-  if x == 1
-    false
-  elsif x < 1
-    false
-  else
-    for i in (2.. i < x/2)
-      if (x%2 == 0)
-        false
-        break
-      else
-        true
-      end
-      i += 1
-    end
-  end
+  array = (2.. x/2)
 end
